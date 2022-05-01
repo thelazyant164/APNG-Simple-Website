a79a67db8e49039de851757e0924e6227c61d204
Lý Thiên Ân <lythienan164@gmail.com>
2022-05-01 21:33:19 +1000

    [A] - added grading, attempt no & datetime

--- assign3/submission.php
+++ assign3/submission.php
@@ -12,25 +12,52 @@
                 return false;
         }
     }
-    function validate_all($allVar) {
+    function validate_all($all_var) {
         #If access without going through quiz.php, redirects back
-        if (!isset($allVar["first_name"])
-         or !isset($allVar["last_name"])
-         or !isset($allVar["student_id"])
-         or !isset($allVar["support_for"])
-         or !isset($allVar["year_developed"])
-         or !isset($allVar["browser_support[]"])
-         or !isset($allVar["developer"])
-         or !isset($allVar["long_name"])) {
+        if (!isset($all_var["first_name"])
+         or !isset($all_var["last_name"])
+         or !isset($all_var["student_id"])
+         or !isset($all_var["support_for"])
+         or !isset($all_var["year_developed"])
+         or !isset($all_var["browser_support[]"])
+         or !isset($all_var["developer"])
+         or !isset($all_var["long_name"])) {
             header("location: quiz.php");
         }
-        return validate_data($allVar["first_name"], "name")
-           and validate_data($allVar["last_name"], "name")
-           and validate_data($allVar["student_id"], "id")
-           and validate_data($allVar["support_for"], "required")
-           and validate_data($allVar["year_developed"], "required");
+        return validate_data($all_var["first_name"], "name")
+           and validate_data($all_var["last_name"], "name")
+           and validate_data($all_var["student_id"], "id")
+           and validate_data($all_var["support_for"], "required")
+           and validate_data($all_var["year_developed"], "required");
     }
     validate_all($_POST);
+
+    #Marking
+    function mark_all($support_for, $year_developed, $browser_support, $developer, $long_name) {
+        $score = 0;
+        if ($support_for == "transparency") {
+            $score += 10;
+        }
+        if ($year_developed == 2004) {
+            $score += 20;
+        }
+        if (in_array("Chrome", $browser_support)) {
+            $score += 5;
+        }
+        if (in_array("Edge", $browser_support)) {
+            $score += 5;
+        }
+        if (in_array("Opera", $browser_support)) {
+            $score += 5;
+        }
+        if ($developer == "Mozilla") {
+            $score += 5;
+        }
+        if ($long_name == "Animated Portable Network Graphics") {
+            $score += 50;
+        }
+        return $score;
+    }
 
     #Import database information, password, username and other config
     require_once("settings.php");
@@ -44,7 +71,7 @@
 
     #Connection succeeds
     } else {
-        $sql_table = "quiz_answers";
+        $sql_table = "attempts";
 
         #Creates new table if table doesn't exist yet
         $query_create = "CREATE TABLE IF NOT EXISTS " . $sql_table . " (
@@ -56,7 +83,10 @@
             year_developed VARCHAR(4) NOT NULL,
             browser_support SET('Chrome', 'Edge', 'Opera', 'Internet Explorer'),
             developer VARCHAR(9),
-            long_name VARCHAR(1000)
+            long_name VARCHAR(1000),
+            attempt_no INT NOT NULL,
+            score INT NOT NULL,
+            date_attempt DATETIME NOT NULL
         )";
         $result_create = mysqli_query($conn, $query_create);
 
@@ -66,15 +96,38 @@
         $student_id = htmlspecialchars(trim($_POST["student_id"]));
         $support_for = htmlspecialchars(trim($_POST["support_for"]));
         $year_developed = htmlspecialchars(trim($_POST["year_developed"]));
-        $browser_support = htmlspecialchars(trim($_POST["browser_support[]"]));
+        $browser_support = [];
+        for ($i = 0; $i < count($_POST["browser_support[]"]); $i++) {
+            array_push($browser_support, htmlspecialchars(trim($_POST["browser_support[]"][$i])));
+        }
         $developer = htmlspecialchars(trim($_POST["developer"]));
         $long_name = htmlspecialchars(trim($_POST["long_name"]));
+        $date_attempt = date('Y-m-d H:i:s');
+
+        #Determine attempt count
+        $query_find = "SELECT * FROM $sql_table
+                       WHERE first_name = '$first_name'
+                         AND last_name = '$last_name'
+                         AND student_id = '$student_id'";
+        $result_find = mysqli_query($conn, $query_find);
+        if (mysqli_num_rows($result_find) == 0) {
+            $attempt_no = 1;
+        } else if (mysqli_num_rows($result_find) == 1) {
+            $attempt_no = 2;
+        } else {
+            exit("Max attempt count exceeded.");
+        }
+
+        #Determine score
+        $score = mark_all($support_for, $year_developed, $browser_support, $developer, $long_name);
 
         #Define INSERT query
         $query_insert = "INSERT INTO $sql_table
-                  (first_name, last_name, student_id, support_for, year_developed, browser_support, developer, long_name)
+                  (first_name, last_name, student_id, support_for, year_developed,
+                  browser_support, developer, long_name, attempt_no, score, date_attempt)
                   VALUES
-                  ('$first_name', '$last_name', '$student_id', '$support_for', '$year_developed', '$browser_support', '$developer', '$long_name')";
+                  ('$first_name', '$last_name', '$student_id', '$support_for', '$year_developed',
+                  '$browser_support', '$developer', '$long_name', '$attempt_no', '$score', '$date_attempt')";
         
         #Perform INSERT query
         $result_insert = mysqli_query($conn, $query_insert);
@@ -82,8 +135,9 @@
         #Server responds
         if (!$result_create) {
             echo "<p>Something is wrong with $query_create.</p>";
-        }
-        else if (!$result_insert) {
+        } else if (!$result_find) {
+            echo "<p>Something is wrong with $query_find.</p>";
+        } else if (!$result_insert) {
             echo "<p>Something is wrong with $query_insert.</p>";
         } else {
             #Right now, if successful submit, return what has just been submitted
