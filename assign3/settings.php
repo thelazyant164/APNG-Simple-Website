e57021dc617f27fe861676074c42d781ee4a7f02
Lý Thiên Ân <lythienan164@gmail.com>
2022-05-04 10:56:18 +1000

    [A] - added development temporary database

--- assign3/settings.php
+++ assign3/settings.php
@@ -1,6 +1,13 @@
 <?php
-    $host = "feenix-mariadb.swin.edu.au";
-    $user = "s103499753";
-    $pwd = "160403";
-    $sql_db = "s103499753_db";
+    #PRODUCTION
+    // $host = "feenix-mariadb.swin.edu.au";
+    // $user = "s103499753";
+    // $pwd = "160403";
+    // $sql_db = "s103499753_db";
+
+    #DEVELOPMENT
+    $host = "sql6.freemysqlhosting.net";
+    $user = "sql6489905";
+    $pwd = "x2dHMBHQT9";
+    $sql_db = "sql6489905";
 ?>
