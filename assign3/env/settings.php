375ebe5bcf53a4593236c51279f1cc1d625dddf4
Ân Thiên Lý <87508573+thelazyant164@users.noreply.github.com>
2022-06-29 17:21:37 +1000

    [C] - censor hard-coded credentials
    Substitute sensitive database credentials in env/settings.php for placeholder

--- assign3/env/settings.php
+++ assign3/env/settings.php
@@ -1,9 +1,9 @@
 <?php
     #PRODUCTION
     $host = "feenix-mariadb.swin.edu.au";
-    $user = "s103499753";
-    $pwd = "160403";
-    $sql_db = "s103499753_db";
+    $user = "studentID";
+    $pwd = "password";
+    $sql_db = "studentID_database";
     
     // #ADMIN credentials
     // $username = "cos10026_assignment3";
